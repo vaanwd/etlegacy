2a4222492292798d32cd33b2e0252df41ee1e4cd
RaFaL <rafal1137@yahoo.pl>
2022-11-29 18:09:09 +0100

    Revert "sdl: enable synthethic mouse movement"
    This reverts commit 64c5dfe999e9c35bd0217760ae499a6a5d609d41.

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -1543,7 +1543,7 @@ void IN_Init(void)
 	// SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, "1");
 	// This has been removed and replaced with those bellow in SDL 2.0.10
 	SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
-	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "1");
+	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
 	SDL_SetHint(SDL_HINT_ANDROID_TRAP_BACK_BUTTON, "1");
 #endif
 
