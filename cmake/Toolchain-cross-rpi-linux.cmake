0b15b4db1911c0a6775e03dddbaa482a1bde53f4
RaFaL <rafal1137@yahoo.pl>
2023-01-16 21:07:30 +0100

    cmake: update toolchain to use aarch64

--- cmake/Toolchain-cross-rpi-linux.cmake
+++ cmake/Toolchain-cross-rpi-linux.cmake
@@ -1,46 +1,42 @@
 #-----------------------------------------------------------------
-# Toolchain Cross RPI 2/3/4 onlyarmv7
+# Toolchain Cross aarch64 based upon raspios rootfs
 #-----------------------------------------------------------------
 
 if("$ENV{RASPBIAN_ROOTFS}" STREQUAL "")
 	message(FATAL_ERROR "Define the RASPBIAN_ROOTFS environment variable to point to the raspbian rootfs.")
+else()
+	set(SYSROOT_PATH "$ENV{RASPBIAN_ROOTFS}")
 endif()
 
 set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR armv7l)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(tools /opt/cross-pi-gcc) # warning change toolchain path here.
 
-set(CMAKE_SYSROOT "$ENV{RASPBIAN_ROOTFS}")
-set(CMAKE_FIND_ROOT_PATH "${CMAKE_PREFIX_PATH};${CMAKE_SYSROOT}")
+set(CMAKE_FIND_ROOT_PATH ${SYSROOT_PATH})
+#set(CMAKE_SYSROOT ${SYSROOT_PATH})
 
-set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+set(CMAKE_LIBRARY_ARCHITECTURE aarch64-linux-gnu)
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fPIC -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} -L${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}")
-set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} -L${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} -L${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} -L${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}")
 
-## Compiler Binary 
-SET(BIN_PREFIX ${tools}/bin/arm-linux-gnueabihf)
+## Compiler Binary
+SET(BIN_PREFIX ${tools}/bin/aarch64-none-linux-gnu)
 
-SET (CMAKE_C_COMPILER ${BIN_PREFIX}-gcc)
-SET (CMAKE_CXX_COMPILER ${BIN_PREFIX}-g++ )
-SET (CMAKE_LINKER ${BIN_PREFIX}-ld 
-            CACHE STRING "Set the cross-compiler tool LD" FORCE)
-SET (CMAKE_AR ${BIN_PREFIX}-ar 
-            CACHE STRING "Set the cross-compiler tool AR" FORCE)
-SET (CMAKE_NM {BIN_PREFIX}-nm 
-            CACHE STRING "Set the cross-compiler tool NM" FORCE)
-SET (CMAKE_OBJCOPY ${BIN_PREFIX}-objcopy 
-            CACHE STRING "Set the cross-compiler tool OBJCOPY" FORCE)
-SET (CMAKE_OBJDUMP ${BIN_PREFIX}-objdump 
-            CACHE STRING "Set the cross-compiler tool OBJDUMP" FORCE)
-SET (CMAKE_RANLIB ${BIN_PREFIX}-ranlib 
-            CACHE STRING "Set the cross-compiler tool RANLIB" FORCE)
-SET (CMAKE_STRIP {BIN_PREFIX}-strip 
-            CACHE STRING "Set the cross-compiler tool RANLIB" FORCE)
+set(CMAKE_C_COMPILER ${BIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${BIN_PREFIX}-g++)
+set(CMAKE_LINKER ${BIN_PREFIX}-ld CACHE STRING "setthe cross-compiler tool LD" FORCE)
+set(CMAKE_AR ${BIN_PREFIX}-ar CACHE STRING "setthe cross-compiler tool AR" FORCE)
+set(CMAKE_NM {BIN_PREFIX}-nm CACHE STRING "setthe cross-compiler tool NM" FORCE)
+set(CMAKE_OBJCOPY ${BIN_PREFIX}-objcopy CACHE STRING "setthe cross-compiler tool OBJCOPY" FORCE)
+set(CMAKE_OBJDUMP ${BIN_PREFIX}-objdump CACHE STRING "setthe cross-compiler tool OBJDUMP" FORCE)
+set(CMAKE_RANLIB ${BIN_PREFIX}-ranlib CACHE STRING "setthe cross-compiler tool RANLIB" FORCE)
+set(CMAKE_STRIP {BIN_PREFIX}-strip CACHE STRING "setthe cross-compiler tool RANLIB" FORCE)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
